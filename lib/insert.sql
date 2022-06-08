81a5e4941c965136cba731beb46c5d4fe6862752
JedwardMook <jedwardmook@yahoo.com>
2022-06-08 12:50:21 -0500

    commit tree 73432c97d1c5593e54baed8523b0c426beedc1b9

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 19, "M", "chocolate brown", "docile", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 11, "M", "sandy brown", "rowdy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 37, "F", "brown", "aggressive", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 66, "F", "white", "timid", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 26, "F", "tan", "friendly", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 7, "M", "green", "greedy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 37, "F", "dark brown", "chill", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 50, "F", "striped black and white", "wise", 0);
+
